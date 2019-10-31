9c32a2b5e4fecda6cc7147329b9ecf3e8267b5b2
AdrielAlmecija <adriel@almecija.com.ar>
2019-10-31 20:55:18 -0300

    cambios en el disenio
    Se agrego jCalendar y Spinner para la cantidad

---
+++ src/PASAJE.java
@@ -0,0 +1,18 @@
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
+public class PASAJE {
+    
+    protected Date Salida = new Date();
+    protected Date Llegada = new Date();
+    protected int numButaca;
+    protected String Origen;
+    protected String Destino;
+    protected int Cantidad;
+     
+    
+    
+}
